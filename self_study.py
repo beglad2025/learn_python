07250a7613096854429fef21b7a0e89cd1839f5c
HEUHYUN <hyun2471215@gmail.com>
2021-02-05 18:37:56 +0900

    day28

--- self_study.py
+++ self_study.py
@@ -89,3 +89,46 @@
 # N, X = input().split()
 # res = [str(i) for i in list(map(int, input().split())) if i < int(X)]
 # print(" ".join(res))
+
+# while 문 문제
+
+# 1. A+B-5
+# while True:
+#     x, y = input().split()
+#     x = int(x)
+#     y = int(y)
+#     if x == 0 and y == 0:
+#         break
+#     print(x + y)
+
+# 2. A+B-4(실패)
+# i = 0
+# while i < 5:
+#     x, y = input().split()
+#     print(int(x) + int(y))
+#     i += 1
+
+# 3. 더하기 사이클
+
+x = input()
+y = int(x)
+i = 0
+while True:
+    i += 1
+    if len(x) == 1:
+        x = "0" + x
+    sumx = str(int(x[0])+int(x[1]))
+    x = x[-1] + sumx[-1]
+    if int(x) == y:
+        break
+print(i)
+
+N = int(input())
+n = -1
+t = 0
+while n !=N:
+    if n == -1:
+        n =N
+    n = (n//10 + n % 10) % 10 + (n % 10) * 10
+    t += 1
+print(t)
