edb97731272e2132fc2e024a3072d8e89044b98c
HEUHYUN <hyun2471215@gmail.com>
2021-02-21 19:29:56 +0900

    self_study_2021

--- self_study_0220.py
+++ self_study_0220.py
@@ -7,8 +7,46 @@
 # print(max(l))
 
 # 2. 최댓값
-l = []
-for i in range(9):
-    l.append(int(input()))
-print(max(l))
-print(l.index(max(l))+1)
+# l = []
+# for i in range(9):
+#     l.append(int(input()))
+# print(max(l))
+# print(l.index(max(l))+1)
+
+# 3. 숫자의 개수
+# a = int(input())
+# b=  int(input())
+# c = int(input())
+# x = str(a * b * c)
+# for i in range(10):
+#     print(x.count(str(i)))
+
+# 4. 나머지
+# l = []
+# for i in range(10):
+#     l.append(int(input()) % 42)
+# print(len(set(l)))
+
+# 5. 평균
+# n = int(input())
+# l = list(map(int, input().split()))
+# mx = max(l)
+# for i in range(n):
+#     l[i] = (l[i] / mx) * 100
+# print(sum(l) / n)
+
+# 6. OX퀴즈
+n = int(input())
+for i in range(n):
+    s = input()
+    res = 0
+    cnt = 0
+    for j in range(len(s)):
+        if s[j] == "O":
+            res += 1
+            if j != 0 and s[j-1] == s[j]:
+                cnt += 1
+                res += cnt
+        else:
+            cnt = 0
+    print(res)
